3c542a314ad71699308f57828b998271a7439587
Michael Adams <mtadams007@gmail.com>
2018-03-20 19:08:00 -0400

    got my pics

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
 $.ajax({
   url: "https://www.redbullshopus.com/products.json",
   success: function(data) {
-    getTitle(data.products);
+    showTitle(data.products);
+    displayImage(data.products);
+    // showImage(data.products);
   }
 })
 
 let getTitle = (arr) => {
-  let i=0;
-  while (i<arr.length) {
+  let i = 0;
+  while (i < arr.length) {
     console.log(arr[i].title);
+    i++;
+  }
+}
+let showTitle = (arr) => {
+  let i = 0;
+  while (i < arr.length) {
+    $('body').append(`<p> ${(arr[i].title)} </p>`);
+    i++;
+  }
+}
+
+let showImage = (arr) => {
+  let i = 0;
+  while(i < arr.length) {
+    console.log(arr[i].images[0].src);
+    i++;
+  }
+}
+
+let displayImage = (arr) => {
+  let i = 0;
+  while(i < arr.length) {
+    $('body').append(`<img src=${arr[i].images[0].src}>`);
     i++;
   }
 }
